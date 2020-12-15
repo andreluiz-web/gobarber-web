25d48929cd152e7cf461943dc69f6bddccb22deb
Rinco1000 <a.rinco@outllok.com>
2020-12-15 00:05:53 -0300

    save files

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import LogoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
     return (
@@ -11,9 +14,19 @@ const SignIn: React.FC = () => {
                 <img src={LogoImg} alt="GoBarber" />
                 <form>
                     <h1>Faca Seu Logon</h1>
-                    <input type="text" placeholder="E-mail" />
-                    <input type="password" placeholder="Password" />
-                    <button type="submit">Entrar</button>
+                    <Input
+                        icon={FiMail}
+                        name="email"
+                        type="text"
+                        placeholder="E-mail"
+                    />
+                    <Input
+                        icon={FiLock}
+                        name="senha"
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <Button type="submit">Entrar</Button>
                     <a href="forgot">Esqueci minha senha</a>
                 </form>
                 <a href="teste">
