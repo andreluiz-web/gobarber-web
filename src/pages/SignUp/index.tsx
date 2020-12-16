2f60c1dbc936367c8523a9102ef8258af0e75262
Rinco1000 <a.rinco@outllok.com>
2020-12-15 23:01:11 -0300

    save files

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,17 +1,30 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import LogoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignUp: React.FC = () => {
-    function handleSubmit(data: string): void {
-        console.log(data);
-    }
+    const handleSubmit = useCallback(async (data: string) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required().email('Email obrigatório'),
+                password: Yup.string().min(6, 'No mínimo 6 digitos'),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
 
     return (
         <Container>
