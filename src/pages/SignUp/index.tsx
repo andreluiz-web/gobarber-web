6dc4291e028d894b381fba97f2fc668a4f10ccb1
Rinco1000 <a.rinco@outllok.com>
2020-12-16 23:33:18 -0300

    save files

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,17 +1,23 @@
 /* eslint-disable react/jsx-indent-props */
 /* eslint-disable react/jsx-indent */
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 import LogoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
+import getValidationErros from '../../utils/getValidationErrors';
 
 const SignUp: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+
     const handleSubmit = useCallback(async (data: string) => {
         try {
+            formRef.current?.setErrors({});
+
             const schema = Yup.object().shape({
                 name: Yup.string().required('Nome obrigatório'),
                 email: Yup.string().required().email('Email obrigatório'),
@@ -22,7 +28,9 @@ const SignUp: React.FC = () => {
                 abortEarly: false,
             });
         } catch (err) {
-            console.log(err);
+            const errors = getValidationErros(err);
+
+            formRef.current?.setErrors(errors);
         }
     }, []);
 
@@ -31,7 +39,7 @@ const SignUp: React.FC = () => {
             <Background />
             <Content>
                 <img src={LogoImg} alt="GoBarber" />
-                <Form onSubmit={handleSubmit}>
+                <Form ref={formRef} onSubmit={handleSubmit}>
                     <h1>Faca Seu Cadastro</h1>
                     <Input
                         icon={FiUser}
