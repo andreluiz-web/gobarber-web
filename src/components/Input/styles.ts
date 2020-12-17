01e2b1393d2f714c1a09c4dfc00a878144f99e53
Rinco1000 <a.rinco@outllok.com>
2020-12-17 10:53:30 -0300

    save files

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tooltip from '../Tooltip/index';
 
 interface ContainerProps {
     isFocused: boolean;
     isFilled: boolean;
+    isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -20,6 +22,12 @@ export const Container = styled.div<ContainerProps>`
     & + div {
         margin-top: 10px;
     }
+
+    ${props =>
+        props.isErrored &&
+        css`
+            border-color: #c53030;
+        `}
 
     ${props =>
         props.isFocused &&
@@ -51,5 +59,23 @@ export const Container = styled.div<ContainerProps>`
 
     svg {
         margin-right: 16px;
+    }
+`;
+
+export const Error = styled(Tooltip)`
+    height: 20px;
+
+    svg {
+        margin: 0;
+        margin-left: 16px;
+    }
+
+    span {
+        background: #c53030;
+        color: #fff;
+
+        &::before {
+            border-color: #c53030 transparent;
+        }
     }
 `;
---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+import { Container } from './styles';
+
+interface TooltipProps {
+    title: string;
+    className?: string;
+}
+
+const Tooltip: React.FC<TooltipProps> = ({
+    title,
+    className = '',
+    children,
+}) => {
+    return (
+        <Container className={className}>
+            {children}
+            <span>{title}</span>
+        </Container>
+    );
+};
+
+export default Tooltip;
