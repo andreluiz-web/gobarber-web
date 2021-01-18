2b0681642c6150378ac505183c7f929e8e22586e
andreluiz-web <a.rinco@outllok.com>
2021-01-18 15:12:54 -0300

    save files

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import DashBoard from '../pages/DashBoard';
 
 const Routes: React.FC = () => {
     return (
         <Switch>
             <Route path="/" exact component={SignIn} />
-            <Route path="/signup" exact component={SignUp} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/dashboard" component={DashBoard} />
         </Switch>
     );
 };
